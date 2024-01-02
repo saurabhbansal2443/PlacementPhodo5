9546920cbc34a95e22b01d8aec49b1e1eaf77f1e
Saurabh Bansal <saurabhbansal2443@gmail.com>
2024-01-02 16:44:57 +0530

    todo list completed

--- DOM/todoList/script.js
+++ DOM/todoList/script.js
@@ -1,6 +1,26 @@
 let inputBar = document.querySelector("input");
 let addBtn = document.querySelector(".add");
+let taskCont= document.querySelector(".taskContainer");
 
 addBtn.addEventListener("click",function(){
-    console.log(inputBar.value);
+    let taskValue = inputBar.value;
+    inputBar.value = "";
+
+    if(taskValue!=""){
+    
+    let myele = document.createElement("div");
+   
+    myele.classList.add("task");
+
+    myele.innerHTML=`<p> ${taskValue} </p> <button> Delete </button>`
+     
+    let delBtn = myele.querySelector("button");
+
+    delBtn.addEventListener("click",function(){
+        taskCont.removeChild(myele);
+    })
+
+     taskCont.appendChild(myele);
+      console.log(myele)
+}
 })
