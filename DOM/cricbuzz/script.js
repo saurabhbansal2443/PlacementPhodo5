9501639dfa52938412a37493bf4df1c63c4bf838
Saurabh Bansal <saurabhbansal2443@gmail.com>
2024-01-03 16:06:19 +0530

    cricbuzz app com[pleted

---
+++ DOM/cricbuzz/script.js
@@ -0,0 +1,26 @@
+let inputBar = document.querySelector("input");
+let selectTag = document.querySelector("select");
+let btn = document.querySelector("button");
+let cont = document.querySelector(".container");
+let colorInput = document.querySelector(".color");
+
+
+btn.addEventListener("click", function () {
+
+    let inputvalue = inputBar.value;
+    inputBar.value = "";
+    let selectValue = selectTag.value;
+    let color = colorInput.value;
+
+    cont.innerHTML = "";
+
+    for (let i = 1; i <= inputvalue; i++) {
+        let ele = document.createElement("div");
+
+        ele.classList.add(selectValue);
+        ele.style.backgroundColor=color;
+
+        cont.appendChild(ele);
+    }
+
+})
