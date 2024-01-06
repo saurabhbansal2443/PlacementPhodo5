8935507a9545c51e7b47d2c5c39306b10f677a99
Saurabh Bansal <saurabhbansal2443@gmail.com>
2024-01-06 15:32:53 +0530

    color swapping

--- DOM/TaskManagerApp/script.js
+++ DOM/TaskManagerApp/script.js
@@ -4,6 +4,11 @@ let taskColors = document.querySelector(".colorPart");
 let activeBtn = document.querySelector(".active");
 let textArea = document.querySelector(".ta");
 let ticketCont = document.querySelector(".ticket-Container");
+let delBtn = document.querySelector(".delbtn");
+
+let colorsArray =["red" ,"yellow" , "green" , "blue"];
+
+let delFlag = false;
 
 addBtn.addEventListener("click", function () {
     taskAdder.classList.toggle("hidden");
@@ -26,15 +31,57 @@ textArea.addEventListener("keydown" , function(event){
       let task = textArea.value ;
       let activeColor =  activeBtn.classList[1];
 
+      // creating new tickets 
+
       let newTicket = document.createElement("div");
       newTicket.classList.add("ticket");
       newTicket.innerHTML=` <div class="ticket-color ${activeColor}"></div>
       <div class="ticket-task">${task}</div>`
+
+    //   delete function of ticket 
+
+      newTicket.addEventListener("dblclick",function(){
+
+        if(delFlag == true){
+            ticketCont.removeChild(newTicket);
+        }
+
+      })
+
+      // color swapping function of ticket 
+
+      let ticketColorContainer = newTicket.querySelector(".ticket-color");
+
+      ticketColorContainer.addEventListener("click",function(){
+            
+        let intialColor = ticketColorContainer.classList[1];
+
+          let colorIndex = colorsArray.indexOf(intialColor);
+        //   console.log(colorIndex);
+        let nextColorIndex = (colorIndex +1)%4 ;
+
+        ticketColorContainer.classList.remove(intialColor);
+        ticketColorContainer.classList.add(colorsArray[nextColorIndex]);
+        
+
+      })
+
      
        ticketCont.appendChild(newTicket);
 
       textArea.value="";
       taskAdder.classList.toggle("hidden");
    }
+})
+
+
+delBtn.addEventListener("click",function(){
+    if(delFlag == false){
+        delBtn.style.color = 'red';
+    }else{
+        delBtn.style.color = 'black';
+    }
+
+    delFlag = !delFlag;
 })
 
