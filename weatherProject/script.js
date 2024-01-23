255611ecf5ae4c2ad8ec74c1669327d545855216
Saurabh Bansal <saurabhbansal2443@gmail.com>
2024-01-23 16:08:58 +0530

    weather app started

---
+++ weatherProject/script.js
@@ -0,0 +1,28 @@
+let apikey = "ed8c37818a944c29adf40311232510";
+let cityEle = document.querySelector(".city");
+let tempEle = document.querySelector(".temp");
+let iconEle = document.querySelector(".icon");
+let humidEle = document.querySelector(".hum");
+
+async function getData(city="gwalior"){
+    let data = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apikey}&q=${city}&aqi=no`);
+    data = await data.json();
+    console.log(data);
+    screenUpdate(data);
+};
+
+getData("dubai")
+
+function screenUpdate(obj){
+    let temp = obj.current.temp_c;
+    let cityName = obj.location.name;
+    let humidity = obj.current.humidity;
+    let icon = obj.current.condition.icon;
+
+    // console.table([temp , cityName , humidity , icon])
+
+    tempEle.innerHTML = temp ;
+    cityEle.innerHTML = cityName;
+    humidEle.innerHTML = humidity;
+    iconEle.src = icon;
+}
